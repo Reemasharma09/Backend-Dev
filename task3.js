4a2eda7bcb5d474792cb07b7ebe65be87ef22ab2
Reema sharma <reemasharma4707@gmail.com>
2026-01-20 15:50:12 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,22 @@
+const fs=require("fs");
+//fs.copyFile("test.txt",new_test.text)
+//fs.copyFileSync("test.txt","dest.txt")
+fs.copyFile("test.txt","new_test.txt",(err)=>{
+    if(err){
+        console.log("error while file is copied",err);
+    }
+    else{
+        console.log("file is copied successfully");
+
+    }
+})
+fs.copyFileSync("test.txt","dest.txt")
+console.log("file is copied")
+fs.unlink("dest.txt",(err)=>{
+    if(err){
+        console.log("error while deleting file",err,err)
+    }
+    else{
+        console.log("file is deleted")
+    }
+})
